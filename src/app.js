97342d00a2429d5d26b0503ba6e1dc451f3a05a9
Yan Fung <fungmingyan@hotmail.com>
2022-12-14 23:50:52 +0000

    Added app.js file.

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.status(200).send('Hello World');
+} )
+
+
+
+
+module.exports = app;
+
