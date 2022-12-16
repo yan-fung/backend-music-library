057a2f3d589d5ad2e32e2896458c79b4c940e5e9
Yan Fung <fungmingyan@hotmail.com>
2022-12-16 20:04:49 +0000

    Added the readArtist function in controllers and added artistRouter.get in routes folder.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,8 +4,17 @@ exports.createArtist = async (req, res) => {
     const { name, genre } = req.body
   
     try {
-      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+      const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
       res.status(201).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+
+  exports.readArtist = async (_req, res) => {
+    try {
+      const { rows } = await db.query('SELECT * FROM Artists')
+      res.status(200).json(rows)
     } catch (err) {
       res.status(500).json(err.message)
     }
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,6 +5,7 @@ const artistRouter = express.Router();
 const artistController = require('../controllers/artist');
 
 artistRouter.post('/', artistController.createArtist)
+artistRouter.get('/', artistController.readArtist)
 
 
 module.exports = artistRouter;
