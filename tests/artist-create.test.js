0f3b1178144cbdf0b95b10a278a24013d9422205
Yan Fung <fungmingyan@hotmail.com>
2022-12-16 15:33:50 +0000

    Added db query to afterEach function in helper.js. Imported dotenv and in test-setup.js.

---
+++ tests/artist-create.test.js
@@ -0,0 +1,28 @@
+//tests/artist-create.js
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../src/db/index')
+const app = require('../src/app');
+
+describe('create artist', () => {
+  describe('/artist', () => {
+    describe('POST', () => {
+      it('creates a new artist in the database', async () => {
+        const { status, body } = await request(app).post('/artists').send({
+          name: 'Tame Impala',
+          genre: 'rock',
+        })    
+
+        expect(status).to.equal(201)
+        expect(body.name).to.equal('Tame Impala')
+        expect(body.genre).to.equal('rock')
+
+        const { rows: [ artistData ] } = await db.query(
+          `SELECT * FROM Artists WHERE id = ${body.id}`
+        )
+        expect(artistData.name).to.equal('Tame Impala')
+        expect(artistData.genre).to.equal('rock')
+      });
+    });
+  });
+});
