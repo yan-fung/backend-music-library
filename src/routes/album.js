ad1a215dd687612b9930adf6a40c4d49093cb334
Yan Fung <fungmingyan@hotmail.com>
2022-12-18 21:39:21 +0000

    Using http verbs put and patch to update album and delete to delete album

--- src/routes/album.js
+++ src/routes/album.js
@@ -7,6 +7,9 @@ const albumController = require('../controllers/album')
 albumRouter.post('/:artistId/album', albumController.createAlbum)
 albumRouter.get('/', albumController.readAlbum)
 albumRouter.get('/:albumId', albumController.getAlbumbyId)
+albumRouter.put('/:albumId', albumController.putAlbumbyId)
+albumRouter.patch('/:albumId', albumController.patchAlbumById)
+albumRouter.delete('/:albumId', albumController.deleteAlbumById)
 
 
 module.exports = albumRouter;
